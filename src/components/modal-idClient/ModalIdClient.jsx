83468dcfe6fec898ee7b199c7f1d43420fc49e2c
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-04-16 15:45:19 -0500

    Fix border style in ModalIdClient component

--- src/components/modal-idClient/ModalIdClient.jsx
+++ src/components/modal-idClient/ModalIdClient.jsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import useAuth from "../../hooks/useAuth";
+import {Alert, Button, CircularProgress} from "@mui/material";
 
 const style = {
   position: "absolute",
@@ -11,7 +12,7 @@ const style = {
   width: 400,
   bgcolor: "background.paper",
   boxShadow: 24,
-  border: 'none',
+  border: "none",
   pt: 2,
   px: 4,
   pb: 3,
@@ -19,6 +20,8 @@ const style = {
 
 export default function ModalIdClient({idClient, setIdClient}) {
   const [open, setOpen] = useState(true);
+  const [error, setError] = useState({isError: false, message: ""});
+  const [isLoading, setIsLoading] = useState(false);
   const [id, setId] = useState(null);
 
   const {isAuthenticated} = useAuth();
@@ -29,14 +32,22 @@ export default function ModalIdClient({idClient, setIdClient}) {
     }
   }, [idClient]);
 
-  const handleClose = () => {
+  const handleClose = async () => {
+    setIsLoading(true);
+    try {
+      const findClient = await fetch(`http://localhost:3001/clients/${id}`);
+      const response = await findClient.json();
+    } catch (error) {
+      setError({isError: true, message: "El cliente no existe"});
+      return;
+    } finally {
+      setIsLoading(false);
+    }
 
     if (!isAuthenticated) return;
-
     setOpen(false);
     setIdClient(id);
   };
-
 
   return (
     <div className="mt-10">
@@ -47,7 +58,10 @@ export default function ModalIdClient({idClient, setIdClient}) {
         aria-describedby="parent-modal-description"
       >
         <Box sx={{...style, width: 400}}>
-          <h2 id="parent-modal-title" className="text-bold text-xl text-center text-[#ed1218]">
+          <h2
+            id="parent-modal-title"
+            className="text-bold text-xl text-center text-[#ed1218]"
+          >
             Ingresa el numero del documento de identidad
           </h2>
           <section
@@ -64,11 +78,20 @@ export default function ModalIdClient({idClient, setIdClient}) {
           <section className="mt-10 flex justify-center">
             <button
               onClick={handleClose}
-              className="bg-[#ed1218] p-3 rounded-3xl text-white"
+              className="bg-[#ed1218] p-3 w-20 rounded-3xl text-white flex items-center justify-center"
             >
-              Ingresar
+              {isLoading ? (
+                <CircularProgress size={20} color="inherit" />
+              ) : (
+                "Ingresar"
+              )}
             </button>
           </section>
+          {error.isError && (
+            <Alert className="mt-5" severity="error">
+              {error.message}
+            </Alert>
+          )}
         </Box>
       </Modal>
     </div>
