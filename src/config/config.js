6aff8ecd272db31eea9a39375c28b2f3205f3154
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-05-28 14:43:53 -0500

    refactor: Update baseUrl in config.js to use VITE_REACT_APP_BASE_BE

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,5 @@
 export const config = {
   host: import.meta.env.VITE_REACT_APP_HOST_BE,
   port: import.meta.env.VITE_REACT_APP_PORT_BE,
-  baseUrl: `${import.meta.env.VITE_REACT_APP_HOST_BE}:${
-    import.meta.env.VITE_REACT_APP_PORT_BE
-  }`,
+  baseUrl: import.meta.env.VITE_REACT_APP_BASE_BE,
 };
